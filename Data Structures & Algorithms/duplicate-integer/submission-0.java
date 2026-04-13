94f0b97e30f0abeaa3d280865fabc77f201e7ff1
Rohit kumar Arya <45693590+rk10kumarrohit@users.noreply.github.com>
2026-04-14 00:25:51 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> numsMap = new HashMap<Integer,Integer>();
+        for (int i= 0; i< nums.length; i++) {
+            if(numsMap.containsValue(nums[i]))
+                return true;
+            else {
+                numsMap.put(i,nums[i]);
+            }   
+        }
+        return false;
+    }
+}
